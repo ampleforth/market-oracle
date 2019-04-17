570776108564fb34a0ce0acb0488d1db086031f5
ahnaguib <ahnaguib@gmail.com>
2019-04-16 19:37:04 -0700

    Oracle Report Delay

--- contracts/MarketOracle.sol
+++ contracts/MarketOracle.sol
@@ -28,24 +28,43 @@ contract MedianOracle is Ownable, IOracle {
     // Addresses of providers authorized to push reports.
     address[] public providers;
 
-    // Reports indexed by provider address. Report.timestamp > 0 indicates entry
-    // existence.
-    mapping (address => Report) public providerReports;
+    // Reports indexed by provider address. Report[0].timestamp > 0
+    // indicates provider existence.
+    mapping (address => Report[2]) public providerReports;
 
     event ProviderAdded(address provider);
     event ProviderRemoved(address provider);
-    event ReportExpired(address provider);
+    event ReportTimestampOutOfRange(address provider);
 
     // The number of seconds after which the report is deemed expired.
-    uint256 public reportExpirationTimeSec = 6 hours;
+    uint256 public reportExpirationTimeSec;
+
+    uint256 public reportDelaySec;
 
     uint256 public minimumProviders = 1;
+
+    constructor(uint256 reportExpirationTimeSec_,
+                uint256 reportDelaySec_,
+                uint256 minimumProviders_)
+    public
+    {
+        reportExpirationTimeSec = reportExpirationTimeSec_;
+        reportDelaySec = reportDelaySec_;
+        minimumProviders = minimumProviders_;
+    }
 
     function setReportExpirationTimeSec(uint256 reportExpirationTimeSec_)
     external
     onlyOwner
     {
         reportExpirationTimeSec = reportExpirationTimeSec_;
+    }
+
+    function setReportDelaySec(uint256 reportDelaySec_)
+    external
+    onlyOwner
+    {
+        reportDelaySec = reportDelaySec_;
     }
 
     function setMinimumProviders(uint256 minimumProviders_)
@@ -58,10 +77,28 @@ contract MedianOracle is Ownable, IOracle {
 
     function pushReport(uint256 payload) external
     {
-        address sender = msg.sender;
-        require(providerReports[sender].timestamp > 0);
-        providerReports[sender].timestamp = now;
-        providerReports[sender].payload = payload;
+        address providerAddress = msg.sender;
+        Report[2] storage reports = providerReports[providerAddress];
+        uint256[2] memory timestamps = [reports[0].timestamp, reports[1].timestamp];
+
+        require(timestamps[0] > 0);
+
+        uint8 index_recent = timestamps[0] >= timestamps[1] ? 0 : 1;
+        uint8 index_past = 1 - index_recent;
+
+        // Check that the push is not too soon after the last one.
+        require(timestamps[index_recent].add(reportDelaySec) <= now);
+
+        reports[index_past].timestamp = now;
+        reports[index_past].payload = payload;
+    }
+
+    function purgeReports() external
+    {
+        address providerAddress = msg.sender;
+        require (providerReports[providerAddress][0].timestamp > 0);
+        providerReports[providerAddress][0].timestamp=1;
+        providerReports[providerAddress][1].timestamp=1;
     }
 
     function getData()
@@ -72,13 +109,37 @@ contract MedianOracle is Ownable, IOracle {
         uint256[] memory validReports = new uint256[](reportsCount);
         uint256 size = 0;
         uint256 minValidTimestamp =  now.sub(reportExpirationTimeSec);
+        uint256 maxValidTimestamp =  now.sub(reportDelaySec);
 
         for (uint256 i = 0; i < reportsCount; i++) {
             address providerAddress = providers[i];
-            if (minValidTimestamp <= providerReports[providerAddress].timestamp) {
-                validReports[size++] = providerReports[providerAddress].payload;
+            Report[2] memory reports = providerReports[providerAddress];
+
+            uint8 index_recent = reports[0].timestamp >= reports[1].timestamp ? 0 : 1;
+            uint8 index_past = 1 - index_recent;
+            uint256 reportTimestampRecent = reports[index_recent].timestamp;
+            if (reportTimestampRecent > maxValidTimestamp) {
+                // Recent report is too recent.
+                uint256 reportTimestampPast = providerReports[providerAddress][index_past].timestamp;
+                if (reportTimestampPast < minValidTimestamp) {
+                    // Past report is too old.
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else if (reportTimestampPast > maxValidTimestamp) {
+                    // Past report is too recent.
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else {
+                    // Using past report.
+                    validReports[size++] = providerReports[providerAddress][index_past].payload;
+                }
             } else {
-                emit ReportExpired(providerAddress);
+                // Recent report is not too recent.
+                if (reportTimestampRecent < minValidTimestamp) {
+                    // Recent report is too old.
+                    emit ReportTimestampOutOfRange(providerAddress);
+                } else {
+                    // Using recent report.
+                    validReports[size++] = providerReports[providerAddress][index_recent].payload;
+                }
             }
         }
 
@@ -97,9 +158,9 @@ contract MedianOracle is Ownable, IOracle {
         external
         onlyOwner
     {
-        require(providerReports[provider].timestamp == 0);
+        require(providerReports[provider][0].timestamp == 0);
         providers.push(provider);
-        providerReports[provider].timestamp = 1;
+        providerReports[provider][0].timestamp = 1;
         emit ProviderAdded(provider);
     }
 
@@ -108,7 +169,7 @@ contract MedianOracle is Ownable, IOracle {
      * @param provider Address of the provider.
      */
     function removeProvider(address provider)
-        public
+        external
         onlyOwner
     {
         delete providerReports[provider];
--- test/unit/market_oracle.js
+++ test/unit/market_oracle.js
@@ -17,8 +17,7 @@ async function setupContractsAndAccounts (accounts) {
   B = accounts[2];
   C = accounts[3];
   D = accounts[4];
-  oracle = await MedianOracle.new();
-  oracle.setReportExpirationTimeSec(3600);
+  oracle = await MedianOracle.new(60,10,1);
 }
 
 contract('MedianOracle:providersSize', async function (accounts) {
@@ -52,9 +51,7 @@ contract('MedianOracle:addProvider', async function (accounts) {
       (await oracle.providersSize.call()).should.be.bignumber.eq(1);
     });
     it('should not add an existing source to the whitelist', async function () {
-      expect(
-        await chain.isEthException(oracle.addProvider(A, { from: deployer }))
-      ).to.be.true;
+      expect(await chain.isEthException(oracle.addProvider(A, { from: deployer }))).to.be.true;
     });
   });
 });
@@ -197,6 +194,7 @@ contract('MedianOracle:getData', async function (accounts) {
     await oracle.pushReport(1041000000000000000, { from: B });
     await oracle.pushReport(1053200000000000000, { from: A });
     await oracle.pushReport(2041000000000000000, { from: C });
+    await chain.waitForSomeTime(10);
   });
 
   describe('when the reports are valid', function () {
@@ -218,23 +216,87 @@ contract('MedianOracle:getData', async function (accounts) {
       await oracle.addProvider(D);
 
       await oracle.pushReport(2041000000000000000, { from: C });
-      await chain.waitForSomeTime(3601);
+      await chain.waitForSomeTime(61);
       await oracle.pushReport(1041000000000000000, { from: B });
       await oracle.pushReport(1000000000000000000, { from: D });
       await oracle.pushReport(1053200000000000000, { from: A });
+      await chain.waitForSomeTime(10);
     });
 
-    it('should emit ReportExpired message', async function () {
+    it('should emit ReportTimestampOutOfRange message', async function () {
       const resp = await oracle.getData();
       const logs = resp.logs;
       const event = logs[0];
-      expect(event.event).to.eq('ReportExpired');
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
       expect(event.args.provider).to.eq(C);
     });
     it('should calculate the exchange rate', async function () {
       const resp = await oracle.getData.call();
       expect(resp[1]).to.be.true;
       resp[0].should.be.bignumber.eq(1041000000000000000);
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  describe('when one of the reports is too recent', function () {
+    before(async function () {
+      await setupContractsAndAccounts(accounts);
+      await oracle.addProvider(A);
+      await oracle.addProvider(B);
+      await oracle.addProvider(C);
+      await oracle.addProvider(D);
+
+      await oracle.pushReport(2041000000000000000, { from: C });
+      await oracle.pushReport(1000000000000000000, { from: D });
+      await oracle.pushReport(1053200000000000000, { from: A });
+      await chain.waitForSomeTime(10);
+      await oracle.pushReport(1041000000000000000, { from: B });
+    });
+
+    it('should emit ReportTimestampOutOfRange message', async function () {
+      const resp = await oracle.getData();
+      const logs = resp.logs;
+      const event = logs[0];
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
+      expect(event.args.provider).to.eq(B);
+    });
+    it('should calculate the exchange rate', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.true;
+      resp[0].should.be.bignumber.eq(1053200000000000000);
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  describe('when not enough providers are valid', function () {
+    before(async function () {
+      await setupContractsAndAccounts(accounts);
+      await oracle.addProvider(A);
+      await oracle.addProvider(B);
+      await oracle.addProvider(C);
+      await oracle.addProvider(D);
+      oracle.setMinimumProviders(4);
+
+      await oracle.pushReport(2041000000000000000, { from: C });
+      await oracle.pushReport(1000000000000000000, { from: D });
+      await oracle.pushReport(1053200000000000000, { from: A });
+      await chain.waitForSomeTime(10);
+      await oracle.pushReport(1041000000000000000, { from: B });
+    });
+
+    it('should emit ReportTimestampOutOfRange message', async function () {
+      const resp = await oracle.getData();
+      const logs = resp.logs;
+      const event = logs[0];
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
+      expect(event.args.provider).to.eq(B);
+    });
+    it('should calculate the exchange rate', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.false;
+      resp[0].should.be.bignumber.eq(0);
     });
   });
 });
@@ -247,18 +309,18 @@ contract('MedianOracle:getData', async function (accounts) {
       await oracle.addProvider(B);
       await oracle.pushReport(1053200000000000000, { from: A });
       await oracle.pushReport(1041000000000000000, { from: B });
-      await chain.waitForSomeTime(3601);
+      await chain.waitForSomeTime(61);
     });
 
-    it('should emit 2 ReportExpired messages', async function () {
+    it('should emit 2 ReportTimestampOutOfRange messages', async function () {
       const resp = await oracle.getData();
       const logs = resp.logs;
       let event = logs[0];
-      expect(event.event).to.eq('ReportExpired');
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
       expect(event.args.provider).to.eq(A);
 
       event = logs[1];
-      expect(event.event).to.eq('ReportExpired');
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
       expect(event.args.provider).to.eq(B);
     });
     it('should return false and 0', async function () {
@@ -266,5 +328,150 @@ contract('MedianOracle:getData', async function (accounts) {
       expect(resp[1]).to.be.false;
       resp[0].should.be.bignumber.eq(0);
     });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(61);
+    await oracle.pushReport(1200000000000000000, { from: A });
+  });
+
+  describe('when recent is too recent and past is too old', function () {
+    it('should emit ReportTimestampOutOfRange message', async function () {
+      const resp = await oracle.getData();
+      const logs = resp.logs;
+      const event = logs[0];
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
+      expect(event.args.provider).to.eq(A);
+    });
+
+    it('should fail', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.false;
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(10);
+    await oracle.pushReport(1200000000000000000, { from: A });
+    await chain.waitForSomeTime(1);
+    oracle.setReportDelaySec(30);
+  });
+
+  describe('when recent is too recent and past is too recent', function () {
+    it('should emit ReportTimestampOutOfRange message', async function () {
+      const resp = await oracle.getData();
+      const logs = resp.logs;
+      const event = logs[0];
+      expect(event.event).to.eq('ReportTimestampOutOfRange');
+      expect(event.args.provider).to.eq(A);
+    });
+
+    it('should fail', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.false;
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(30);
+    await oracle.pushReport(1200000000000000000, { from: A });
+    await chain.waitForSomeTime(4);
+  });
+
+  describe('when recent is too recent and past is valid', function () {
+    it('should succeeded', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.true;
+      resp[0].should.be.bignumber.eq(1100000000000000000);
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(30);
+    await oracle.pushReport(1200000000000000000, { from: A });
+    await chain.waitForSomeTime(10);
+  });
+
+  describe('when recent is not too recent nor too old', function () {
+    it('should succeed', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.true;
+      resp[0].should.be.bignumber.eq(1200000000000000000);
+    });
+  });
+});
+
+contract('MedianOracle:getData', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(30);
+    await oracle.pushReport(1200000000000000000, { from: A });
+    await chain.waitForSomeTime(80);
+  });
+
+  describe('when recent is not too recent but too old', function () {
+    it('should fail', async function () {
+      const resp = await oracle.getData.call();
+      expect(resp[1]).to.be.false;
+    });
+  });
+});
+
+contract('MedianOracle:PurgeReports', async function (accounts) {
+  before(async function () {
+    await setupContractsAndAccounts(accounts);
+    await oracle.addProvider(A);
+
+    await oracle.pushReport(1100000000000000000, { from: A });
+    await chain.waitForSomeTime(20);
+    await oracle.pushReport(1200000000000000000, { from: A });
+    await chain.waitForSomeTime(20);
+    await oracle.purgeReports({ from: A });
+  });
+
+  it('data not available after purge', async function () {
+    const resp = await oracle.getData.call();
+    expect(resp[1]).to.be.false;
+  });
+  it('data available after another report', async function () {
+    await oracle.pushReport(1300000000000000000, { from: A });
+    await chain.waitForSomeTime(20);
+    const resp = await oracle.getData.call();
+    expect(resp[1]).to.be.true;
+    resp[0].should.be.bignumber.eq(1300000000000000000);
+  });
+
+  it('cannot purge a non-whitelisted provider', async function () {
+    expect(await chain.isEthException(oracle.purgeReports({ from: B }))).to.be.true;
+    expect(await chain.isEthException(oracle.purgeReports({ from: A }))).to.be.false;
+    await oracle.removeProvider(A);
+    expect(await chain.isEthException(oracle.purgeReports({ from: A }))).to.be.true;
   });
 });
